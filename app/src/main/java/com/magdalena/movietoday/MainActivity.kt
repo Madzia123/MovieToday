34b6fc6de26e7898744e49e85027164f18b5d62f
Magdalena Dziesińska <magdalenadziesinska@gmail.com>
2020-08-13 02:04:37 +0200

    add view fragment_movie_list.xml , fragment_movie_details.xml , item_movie_details.xml add navigation

--- app/src/main/java/com/magdalena/movietoday/MainActivity.kt
+++ app/src/main/java/com/magdalena/movietoday/MainActivity.kt
@@ -2,10 +2,24 @@ package com.magdalena.movietoday
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import com.magdalena.movietoday.base.Navigation
+import com.magdalena.movietoday.base.ToolbarInteraction
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), Navigation, ToolbarInteraction {
+    override val topActionBar: ActionBar?
+        get() =  supportActionBar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun getNavigationController(): NavController = findNavController(R.id.nav_host_fragment)
+
+
+
+
 }
